75db414ec3e4fa24e418e3e5bcd7f913388a4890
U-Daniel-PC\Daniel <corrnondacqb@yahoo.com>
2014-07-24 22:17:47 -0500

    Rename folder: trainers -> duelists

--- gfx.asm
+++ gfx.asm
@@ -39,7 +39,7 @@ SolidTiles2: ; 87d3c (21:7d3c)
 
 PlayerGfx: ; 87d7e (21:7d7e)
 	dw $24
-	INCBIN "gfx/trainers/player.2bpp"
+	INCBIN "gfx/duelists/player.2bpp"
 
 Unnamed1Gfx: ; 87fc0 (21:7fc0)
 	dw $2
@@ -197,7 +197,7 @@ Mystery2Gfx: ; 93540 (24:7540)
 
 RonaldGfx: ; 93aa2 (24:7aa2)
 	dw $24
-	INCBIN "gfx/trainers/ronald.2bpp"
+	INCBIN "gfx/duelists/ronald.2bpp"
 
 CopyrightGfx: ; 93ce4 (24:7ce4)
 	dw $24
@@ -306,115 +306,115 @@ Unnamed7Gfx: ; 9bfee (26:7fee)
 SECTION "bank27",ROMX,BANK[$27]
 SamGfx: ; 9c000 (27:4000)
 	dw $24
-	INCBIN "gfx/trainers/sam.2bpp"
+	INCBIN "gfx/duelists/sam.2bpp"
 
 ImakuniGfx: ; 9c242 (27:4242)
 	dw $24
-	INCBIN "gfx/trainers/imakuni.2bpp"
+	INCBIN "gfx/duelists/imakuni.2bpp"
 
 NikkiGfx: ; 9c484 (27:4484)
 	dw $24
-	INCBIN "gfx/trainers/nikki.2bpp"
+	INCBIN "gfx/duelists/nikki.2bpp"
 
 RickGfx: ; 9c6c6 (27:46c6)
 	dw $24
-	INCBIN "gfx/trainers/rick.2bpp"
+	INCBIN "gfx/duelists/rick.2bpp"
 
 KenGfx: ; 9c908 (27:4908)
 	dw $24
-	INCBIN "gfx/trainers/ken.2bpp"
+	INCBIN "gfx/duelists/ken.2bpp"
 
 AmyGfx: ; 9cb4a (27:4b4a)
 	dw $24
-	INCBIN "gfx/trainers/amy.2bpp"
+	INCBIN "gfx/duelists/amy.2bpp"
 
 IsaacGfx: ; 9cd8c (27:4d8c)
 	dw $24
-	INCBIN "gfx/trainers/isaac.2bpp"
+	INCBIN "gfx/duelists/isaac.2bpp"
 
 MitchGfx: ; 9cfce (27:4fce)
 	dw $24
-	INCBIN "gfx/trainers/mitch.2bpp"
+	INCBIN "gfx/duelists/mitch.2bpp"
 
 GeneGfx: ; 9d210 (27:5210)
 	dw $24
-	INCBIN "gfx/trainers/gene.2bpp"
+	INCBIN "gfx/duelists/gene.2bpp"
 
 MurrayGfx: ; 9d452 (27:5452)
 	dw $24
-	INCBIN "gfx/trainers/murray.2bpp"
+	INCBIN "gfx/duelists/murray.2bpp"
 
 CourtneyGfx: ; 9d694 (27:5694)
 	dw $24
-	INCBIN "gfx/trainers/courtney.2bpp"
+	INCBIN "gfx/duelists/courtney.2bpp"
 
 SteveGfx: ; 9d8d6 (27:58d6)
 	dw $24
-	INCBIN "gfx/trainers/steve.2bpp"
+	INCBIN "gfx/duelists/steve.2bpp"
 
 JackGfx: ; 9db18 (27:5b18)
 	dw $24
-	INCBIN "gfx/trainers/jack.2bpp"
+	INCBIN "gfx/duelists/jack.2bpp"
 
 RodGfx: ; 9dd5a (27:5d5a)
 	dw $24
-	INCBIN "gfx/trainers/rod.2bpp"
+	INCBIN "gfx/duelists/rod.2bpp"
 
 JosephGfx: ; 9df9c (27:5f9c)
 	dw $24
-	INCBIN "gfx/trainers/joseph.2bpp"
+	INCBIN "gfx/duelists/joseph.2bpp"
 
 DavidGfx: ; 9e1de (27:61de)
 	dw $24
-	INCBIN "gfx/trainers/david.2bpp"
+	INCBIN "gfx/duelists/david.2bpp"
 
 ErikGfx: ; 9e420 (27:6420)
 	dw $24
-	INCBIN "gfx/trainers/erik.2bpp"
+	INCBIN "gfx/duelists/erik.2bpp"
 
 JohnGfx: ; 9e662 (27:6662)
 	dw $24
-	INCBIN "gfx/trainers/john.2bpp"
+	INCBIN "gfx/duelists/john.2bpp"
 
 AdamGfx: ; 9e8a4 (27:68a4)
 	dw $24
-	INCBIN "gfx/trainers/adam.2bpp"
+	INCBIN "gfx/duelists/adam.2bpp"
 
 JonathanGfx: ; 9eae6 (27:6ae6)
 	dw $24
-	INCBIN "gfx/trainers/jonathan.2bpp"
+	INCBIN "gfx/duelists/jonathan.2bpp"
 
 JoshuaGfx: ; 9ed28 (27:6d28)
 	dw $24
-	INCBIN "gfx/trainers/joshua.2bpp"
+	INCBIN "gfx/duelists/joshua.2bpp"
 
 NicholasGfx: ; 9ef6a (27:6f6a)
 	dw $24
-	INCBIN "gfx/trainers/nicholas.2bpp"
+	INCBIN "gfx/duelists/nicholas.2bpp"
 
 BrandonGfx: ; 9f1ac (27:71ac)
 	dw $24
-	INCBIN "gfx/trainers/brandon.2bpp"
+	INCBIN "gfx/duelists/brandon.2bpp"
 
 MatthewGfx: ; 9f3ee (27:73ee)
 	dw $24
-	INCBIN "gfx/trainers/matthew.2bpp"
+	INCBIN "gfx/duelists/matthew.2bpp"
 
 RyanGfx: ; 9f630 (27:7630)
 	dw $24
-	INCBIN "gfx/trainers/ryan.2bpp"
+	INCBIN "gfx/duelists/ryan.2bpp"
 
 AndrewGfx: ; 9f872 (27:7872)
 	dw $24
-	INCBIN "gfx/trainers/andrew.2bpp"
+	INCBIN "gfx/duelists/andrew.2bpp"
 
 ChrisGfx: ; 9fab4 (27:7ab4)
 	dw $24
-	INCBIN "gfx/trainers/chris.2bpp"
+	INCBIN "gfx/duelists/chris.2bpp"
 
 MichaelGfx: ; 9fcf6 (27:7cf6)
 	dw $24
-	INCBIN "gfx/trainers/michael.2bpp"
+	INCBIN "gfx/duelists/michael.2bpp"
 
 Unnamed8Gfx: ; 9ff38 (27:7f38)
 	dw $a
@@ -431,47 +431,47 @@ endr
 SECTION "bank28",ROMX,BANK[$28]
 DanielGfx: ; a0000 (28:4000)
 	dw $24
-	INCBIN "gfx/trainers/daniel.2bpp"
+	INCBIN "gfx/duelists/daniel.2bpp"
 
 RobertGfx: ; a0242 (28:4242)
 	dw $24
-	INCBIN "gfx/trainers/robert.2bpp"
+	INCBIN "gfx/duelists/robert.2bpp"
 
 BrittanyGfx: ; a0484 (28:4484)
 	dw $24
-	INCBIN "gfx/trainers/brittany.2bpp"
+	INCBIN "gfx/duelists/brittany.2bpp"
 
 KristinGfx: ; a06c6 (28:46c6)
 	dw $24
-	INCBIN "gfx/trainers/kristin.2bpp"
+	INCBIN "gfx/duelists/kristin.2bpp"
 
 HeatherGfx: ; a0908 (28:4908)
 	dw $24
-	INCBIN "gfx/trainers/heather.2bpp"
+	INCBIN "gfx/duelists/heather.2bpp"
 
 SaraGfx: ; a0b4a (28:4b4a)
 	dw $24
-	INCBIN "gfx/trainers/sara.2bpp"
+	INCBIN "gfx/duelists/sara.2bpp"
 
 AmandaGfx: ; a0d8c (28:4d8c)
 	dw $24
-	INCBIN "gfx/trainers/amanda.2bpp"
+	INCBIN "gfx/duelists/amanda.2bpp"
 
 JenniferGfx: ; a0fce (28:4fce)
 	dw $24
-	INCBIN "gfx/trainers/jennifer.2bpp"
+	INCBIN "gfx/duelists/jennifer.2bpp"
 
 JessicaGfx: ; a1210 (28:5210)
 	dw $24
-	INCBIN "gfx/trainers/jessica.2bpp"
+	INCBIN "gfx/duelists/jessica.2bpp"
 
 StephanieGfx: ; a1452 (28:5452)
 	dw $24
-	INCBIN "gfx/trainers/stephanie.2bpp"
+	INCBIN "gfx/duelists/stephanie.2bpp"
 
 AaronGfx: ; a1694 (28:5694)
 	dw $24
-	INCBIN "gfx/trainers/aaron.2bpp"
+	INCBIN "gfx/duelists/aaron.2bpp"
 
 OWSprite0: ; a18d6 (28:58d6)
 	dw $14
