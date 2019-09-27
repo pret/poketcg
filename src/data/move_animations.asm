c88c1b02cca80965eef4b8862e925cf5f03d85d4
jidoc01 <jidoc01@naver.com>
2019-09-27 13:50:45 +0200

    Disassemble move animation data
    The disassembled data is pretty huge, so I moved it into a new
    separated file(data/move_animations.asm).
    I've just named the data labels in a simple way, and it's needed
    to rename them depending on their corresponding move names.

---
+++ src/data/move_animations.asm
@@ -0,0 +1,437 @@
+MoveAnimation_52c6: ; (6:52c6)
+	db $02, $01, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_52cf:
+	db $02, $01, $03, $08, $01, $fb, $03, $09
+	db $00
+
+MoveAnimation_52d8:
+	db $02, $01, $03, $0a, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_52e3:
+	db $02, $01, $03, $0b, $03, $0c, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_52f0:
+	db $02, $01, $01, $65, $01, $0d, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_52fd:
+	db $02, $01, $03, $0e, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5308:
+	db $02, $01, $03, $0f, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5313:
+	db $02, $01, $01, $10, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_531e:
+	db $02, $01, $03, $11, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5329:
+	db $02, $01, $01, $12, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5334:
+	db $02, $01, $03, $13, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_533f:
+	db $02, $01, $01, $14, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_534a:
+	db $02, $01, $03, $0b, $03, $13, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5357:
+	db $02, $01, $03, $15, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5362:
+	db $02, $01, $01, $16, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_536d:
+	db $02, $01, $03, $17, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5378:
+	db $02, $01, $03, $18, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5383:
+	db $02, $01, $03, $19, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_538e:
+	db $02, $01, $03, $1a, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5399:
+	db $02, $01, $03, $1b, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53a4:
+	db $02, $01, $03, $1c, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53af:
+	db $02, $01, $03, $1d, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53ba:
+	db $02, $01, $03, $1e, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53c5:
+	db $02, $01, $03, $1f, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53d0:
+	db $02, $01, $02, $20
+	db $00
+
+MoveAnimation_53d5:
+	db $02, $01, $03, $21, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53e0:
+	db $02, $01, $03, $22, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53eb:
+	db $02, $01, $03, $23, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_53f6:
+	db $02, $01, $03, $24, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5401:
+	db $02, $01, $03, $25, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_540c:
+	db $02, $01, $03, $26, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5417:
+	db $02, $01, $03, $27, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5422:
+	db $02, $01, $03, $28, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_542d:
+	db $02, $01, $03, $29, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5438:
+	db $02, $01, $03, $2a, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5443:
+	db $02, $01, $03, $2b, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_544e:
+	db $02, $01, $03, $2b
+	db $00
+
+MoveAnimation_5453:
+	db $02, $01, $03, $2c, $01, $66, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5460:
+	db $02, $01, $03, $2c, $01, $66
+	db $00
+
+MoveAnimation_5467:
+	db $02, $01, $03, $2d, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5472:
+	db $02, $01, $03, $2e, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_547d:
+	db $02, $01, $03, $2f, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5488:
+	db $02, $01, $02, $30, $01, $66
+	db $00
+
+MoveAnimation_548f:
+	db $02, $01, $01, $66, $03, $31, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_549c:
+	db $02, $01, $01, $65, $03, $32, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_54a9:
+	db $02, $01, $03, $33
+	db $00
+
+MoveAnimation_54ae:
+	db $02, $01, $03, $34
+	db $00
+
+MoveAnimation_54b3:
+	db $02, $01, $01, $35, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_54be:
+	db $02, $01, $02, $36
+	db $00
+
+MoveAnimation_54c3:
+	db $02, $01, $02, $37
+	db $00
+
+MoveAnimation_54c8:
+	db $02, $01, $01, $38, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_54d3:
+	db $02, $01, $01, $38, $03, $07, $01, $fa, $03, $09, $02, $36
+	db $00
+
+MoveAnimation_54e0:
+	db $02, $01, $03, $39, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_54eb:
+	db $02, $01, $02, $3a, $01, $fa
+	db $00
+
+MoveAnimation_54f2:
+	db $02, $01, $01, $fa, $03, $3b
+	db $00
+
+MoveAnimation_54f9:
+	db $02, $01, $02, $3c, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5504:
+	db $02, $01, $02, $3d, $01, $65, $02, $41, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5513:
+	db $02, $01
+	db $00
+
+MoveAnimation_5516:
+	db $02, $01, $03, $3f, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5521:
+	db $02, $01, $03, $40, $03, $07, $01, $fa, $03, $09, $03, $3b
+	db $00
+
+MoveAnimation_552e:
+	db $02, $01, $01, $65
+	db $00
+
+MoveAnimation_5533:
+	db $02, $01, $01, $65, $03, $01
+	db $00
+
+MoveAnimation_553a:
+	db $02, $01, $01, $65, $04, $04, $05, $46
+	db $00
+
+MoveAnimation_5543:
+	db $04, $04, $05, $46, $01, $65
+	db $00
+
+MoveAnimation_554a:
+	db $04, $04, $05, $46, $01, $65, $05, $47, $05, $47
+	db $00
+
+MoveAnimation_5555:
+	db $04, $04, $05, $46, $01, $45, $04, $01
+	db $00
+
+MoveAnimation_555e:
+	db $04, $04, $05, $46, $05, $44, $04, $04, $05, $07, $01, $fa, $05, $09
+	db $00
+
+MoveAnimation_556d:
+	db $04, $04, $05, $46, $05, $49
+	db $00
+
+MoveAnimation_5574:
+	db $02, $01, $04, $04, $01, $4a
+	db $00
+
+MoveAnimation_557b:
+	db $02, $01
+	db $00
+
+MoveAnimation_557e:
+	db $02, $01, $01, $65
+	db $00
+
+MoveAnimation_5583:
+	db $02, $01, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_558c:
+	db $02, $01, $03, $29, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_5597:
+	db $02, $01, $02, $33
+	db $00
+
+MoveAnimation_559c:
+	db $02, $01, $02, $4b
+	db $00
+
+MoveAnimation_55a1:
+	db $02, $01
+	db $00
+
+MoveAnimation_55a4:
+	db $02, $01, $02, $4d
+	db $00
+
+MoveAnimation_55a9:
+	db $02, $01, $03, $22, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_55b4:
+	db $02, $01, $02, $4d, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_55bf:
+	db $02, $01, $03, $2b
+	db $00
+
+MoveAnimation_55c4:
+	db $02, $01, $03, $17
+	db $00
+
+MoveAnimation_55c9:
+	db $02, $01, $01, $65
+	db $00
+
+MoveAnimation_55ce:
+	db $02, $01, $03, $18, $03, $3b
+	db $00
+
+MoveAnimation_55d5:
+	db $02, $01, $02, $04, $02, $07, $01, $fc, $02, $09
+	db $00
+
+MoveAnimation_55e0:
+	db $02, $01, $01, $12
+	db $00
+
+MoveAnimation_55e5:
+	db $00
+
+MoveAnimation_55e6:
+	db $04, $04, $05, $06, $05, $09
+	db $00
+
+MoveAnimation_55ed:
+	db $02, $3e, $02, $09
+	db $00
+
+MoveAnimation_55f2:
+	db $04, $01, $02, $07, $01, $fc, $02, $09
+	db $00
+
+MoveAnimation_55fb:
+	db $03, $05
+	db $00
+
+MoveAnimation_55fe:
+	db $03, $04
+	db $00
+
+MoveAnimation_5601:
+	db $03, $02
+	db $00
+
+MoveAnimation_5604:
+	db $03, $03
+	db $00
+
+MoveAnimation_5607:
+	db $02, $04
+	db $00
+
+MoveAnimation_560a:
+	db $02, $01, $03, $2a
+	db $00
+
+MoveAnimation_560f:
+	db $03, $3b
+	db $00
+
+MoveAnimation_5612:
+	db $04, $04, $05, $44, $05, $07, $01, $fa, $05, $09
+	db $00
+
+MoveAnimation_561d:
+	db $04, $04, $05, $4c, $05, $07, $01, $fa, $05, $09
+	db $00
+
+MoveAnimation_5628:
+	db $04, $04, $01, $4e
+	db $00
+
+MoveAnimation_562d:
+	db $04, $04, $01, $4f
+	db $00
+
+MoveAnimation_5632:
+	db $04, $04, $05, $09
+	db $00
+
+MoveAnimation_5637:
+	db $02, $01, $03, $39, $03, $07, $01, $fa, $03, $09, $01, $65
+	db $00
+
+MoveAnimation_5644:
+	db $02, $01, $02, $4b, $03, $07, $01, $fa, $03, $09
+	db $00
+
+MoveAnimation_564f:
+	db $02, $05, $02, $09
+	db $00
+
+MoveAnimation_5654:
+	db $02, $3e, $01, $98
+	db $00
+
+MoveAnimation_5659:
+	db $02, $03, $01, $98
+	db $00
+
+MoveAnimation_565e:
+	db $02, $01, $03, $2c, $01, $66
+	db $00
+
+MoveAnimation_5665:
+	db $03, $39
+	db $00
+
+MoveAnimation_5668:
+	db $04, $04, $05, $46, $04, $01, $02, $3e, $03, $3e
+	db $00
+
+MoveAnimation_5673:
+	db $00
