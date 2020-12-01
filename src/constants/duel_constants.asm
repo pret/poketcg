6d17f2ebb999e0c42e5eb740bdd92399b5c69dba
pinksylveon <shakibafzali@gmail.com>
2020-12-01 15:31:44 -0500

    Added DBLPSN_DAMAGE constant

--- src/constants/duel_constants.asm
+++ src/constants/duel_constants.asm
@@ -123,9 +123,10 @@ DOUBLE_POISONED_F EQU 6
 POISONED_F        EQU 7
 
 
-CNF_SLP_PRZ  EQU $0f ; confused, asleep or paralyzed
-PSN_DBLPSN   EQU $f0 ; poisoned or double poisoned
-PSN_DAMAGE   EQU 10
+CNF_SLP_PRZ   EQU $0f ; confused, asleep or paralyzed
+PSN_DBLPSN    EQU $f0 ; poisoned or double poisoned
+PSN_DAMAGE    EQU 10
+DBLPSN_DAMAGE EQU $14
 
 ; substatus conditions (DUELVARS_ARENA_CARD_SUBSTATUS*)
 
