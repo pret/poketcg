7e8efd20169d5c58e3ef78f05814932edbe25061
dannye <33dannye@gmail.com>
2022-05-11 22:27:13 -0500

    Update rgbdscheck.asm

--- src/rgbdscheck.asm
+++ src/rgbdscheck.asm
@@ -1,7 +1,7 @@
-; poketcg requires rgbds 0.4.1 or newer.
+; poketcg requires rgbds 0.5.0 or newer.
 DEF MAJOR EQU 0
-DEF MINOR EQU 4
-DEF PATCH EQU 1
+DEF MINOR EQU 5
+DEF PATCH EQU 0
 
 IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
 	fail "poketcg requires rgbds {MAJOR}.{MINOR}.{PATCH} or newer."
