87a2a180a66e2425d74d7f067a4426c303152d3a
ElectroDeoxys <ElectroDeoxys@gmail.com>
2022-12-18 13:52:33 +0000

    Restructure duelist variables as a WRAM struct

--- src/macros/wram.asm
+++ src/macros/wram.asm
@@ -98,3 +98,130 @@ MACRO deck_struct
 \1Name::  ds DECK_NAME_SIZE
 \1Cards:: ds DECK_SIZE
 ENDM
+
+MACRO duel_vars
+	ASSERT (LOW(@) == 0), "must be aligned to $100"
+
+; 60-byte array that indicates where each of the 60 cards is.
+;	$00 - deck
+;	$01 - hand
+;	$02 - discard pile
+;	$08 - prize
+;	$10 - arena (active pokemon or a card attached to it)
+;	$1X - bench (where X is bench position from 1 to 5)
+\1CardLocations::                ds DECK_SIZE
+
+; deck indexes of the up to 6 cards placed as prizes
+\1PrizeCards::                   ds $6
+
+; deck indexes of the cards that are in the duelist's hand
+\1Hand::                         ds DECK_SIZE
+
+; 60-byte array that maps each card to its position in the deck or anywhere else
+; This array is initialized to 00, 01, 02, ..., 59, until deck is shuffled.
+; Cards in the discard pile go first, cards still in the deck go last, and others go in-between.
+\1DeckCards::                    ds DECK_SIZE
+
+; Stores x = (60 - deck remaining cards).
+; The first x cards in the \1DeckCards array are no longer in the drawable deck this duel.
+; The top card of the duelist's deck is at \1DeckCards + [\1NumberOfCardsNotInDeck].
+\1NumberOfCardsNotInDeck::       ds $1
+
+; Deck index of the card that is in duelist's side of the field
+; -1 indicates no pokemon
+\1ArenaCard::                    ds $1
+
+; Deck indexes of the cards that are in duelist's bench, plus an $ff (-1) terminator
+; -1 indicates no pokemon
+\1Bench::                        ds MAX_BENCH_POKEMON + 1
+
+\1ArenaCardFlags::               ds $1
+
+	ds $5
+
+\1ArenaCardHP::                  ds $1
+\1Bench1CardHP::                 ds $1
+\1Bench2CardHP::                 ds $1
+\1Bench3CardHP::                 ds $1
+\1Bench4CardHP::                 ds $1
+\1Bench5CardHP::                 ds $1
+\1ArenaCardStage::               ds $1
+\1Bench1CardStage::              ds $1
+\1Bench2CardStage::              ds $1
+\1Bench3CardStage::              ds $1
+\1Bench4CardStage::              ds $1
+\1Bench5CardStage::              ds $1
+
+; changed type from Venomoth's Shift Pokemon Power
+; if bit 7 == 1, then bits 0-3 override the Pokemon's actual color
+\1ArenaCardChangedType::         ds $1
+\1Bench1CardChangedType::        ds $1
+\1Bench2CardChangedType::        ds $1
+\1Bench3CardChangedType::        ds $1
+\1Bench4CardChangedType::        ds $1
+\1Bench5CardChangedType::        ds $1
+
+\1ArenaCardAttachedDefender::    ds $1
+\1Bench1CardAttachedDefender::   ds $1
+\1Bench2CardAttachedDefender::   ds $1
+\1Bench3CardAttachedDefender::   ds $1
+\1Bench4CardAttachedDefender::   ds $1
+\1Bench5CardAttachedDefender::   ds $1
+
+\1ArenaCardAttachedPluspower::   ds $1
+\1Bench1CardAttachedPluspower::  ds $1
+\1Bench2CardAttachedPluspower::  ds $1
+\1Bench3CardAttachedPluspower::  ds $1
+\1Bench4CardAttachedPluspower::  ds $1
+\1Bench5CardAttachedPluspower::  ds $1
+
+	ds $1
+
+\1ArenaCardSubstatus1::          ds $1
+\1ArenaCardSubstatus2::          ds $1
+
+; changed weakness from Conversion 1
+\1ArenaCardChangedWeakness::     ds $1
+; changed resistance from Conversion 2
+\1ArenaCardChangedResistance::   ds $1
+
+\1ArenaCardSubstatus3::          ds $1
+
+; each bit represents a prize that this duelist can draw (1 = not drawn ; 0 = drawn)
+\1Prizes::                       ds $1
+
+\1NumberOfCardsInDiscardPile::   ds $1
+\1NumberOfCardsInHand::          ds $1
+
+; Pokemon cards in arena + bench
+\1NumberOfPokemonInPlayArea::    ds $1
+
+\1ArenaCardStatus::              ds $1
+
+; $00   - player
+; $01   - link
+; other - AI controlled
+\1DuelistType::                  ds $1
+
+; if under the effects of amnesia, which attack (0 or 1) can't be used
+\1ArenaCardDisabledAttackIndex:: ds $1
+
+; damage taken the last time the opponent attacked (0 if no damage)
+\1ArenaCardLastTurnDamage::      ds $2
+
+; status condition received the last time the opponent attacked (0 if none)
+\1ArenaCardLastTurnStatus::      ds $1
+
+; substatus2 that the opponent card got last turn
+\1ArenaCardLastTurnSubstatus2::  ds $1
+
+; indicates color of weakness that was changed
+; for this card last turn
+\1ArenaCardLastTurnChangeWeak::  ds $1
+
+; stores an effect that was used on the Arena card last turn.
+; see LAST_TURN_EFFECT_* constants.
+\1ArenaCardLastTurnEffect::      ds $1
+
+	ds $7
+ENDM
