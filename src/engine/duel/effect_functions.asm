a8838f6c7ae2e901c20926d19be5a0f0b85b5ac8
Andrzej Kurek <andrzejku@gmail.com>
2023-02-07 12:49:49 +0100

    Fix incorrect reference to DUELVARS instead of Trainer card text Coincidentally, these values are the same for the original game - 0xef.

--- src/engine/duel/effect_functions.asm
+++ src/engine/duel/effect_functions.asm
@@ -10439,7 +10439,7 @@ PokeBall_DeckCheck:
 	ret
 
 PokeBall_PlayerSelection:
-	ld de, DUELVARS_NUMBER_OF_POKEMON_IN_PLAY_AREA
+	ldtx de, TrainerCardSuccessCheckText
 	call Func_2c08a
 	ldh [hTempList], a ; store coin result
 	ret nc
@@ -10524,7 +10524,7 @@ Recycle_DiscardPileCheck:
 	ret
 
 Recycle_PlayerSelection:
-	ld de, DUELVARS_NUMBER_OF_POKEMON_IN_PLAY_AREA
+	ldtx de, TrainerCardSuccessCheckText
 	call Func_2c08a
 	jr nc, .tails
 
