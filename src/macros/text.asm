893732d442011cdf4d4f81f817525cf15f3bacc0
Rangi42 <remy.oukaour+rangi42@gmail.com>
2024-08-04 10:00:20 -0400

    Refactor the `_textfw` macro

--- src/macros/text.asm
+++ src/macros/text.asm
@@ -5,45 +5,43 @@ DEF done EQUS "db TX_END"
 DEF half2full EQUS "db TX_HALF2FULL"
 
 MACRO _textfw
-	PUSHO
-	OPT Wno-unmapped-char
 	REPT _NARG
 		IF STRLEN(\1) > 0
-			IF STRCMP(STRSUB(\1, 1, 1), "<") == 0 && STRLEN(\1) > 1
+			IF !STRCMP(STRSUB(\1, 1, 1), "<") && STRLEN(\1) > 1
 				db \1
 			ELSE
-				FOR i, STRLEN(\1)
-					IF CHARLEN(STRCAT("FW{x:TX_KATAKANA}_", STRSUB(\1, i + 1, 1))) == 1
+				FOR i, 1, STRLEN(\1) + 1
+					REDEF char EQUS STRSUB(\1, i, 1)
+					IF INCHARMAP("FW{x:TX_KATAKANA}_{char}")
 						IF cur_set != TX_KATAKANA
 							DEF cur_set = TX_KATAKANA
 							db cur_set
 						ENDC
-						db STRCAT("FW{x:TX_KATAKANA}_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW{x:TX_HIRAGANA}_", STRSUB(\1, i + 1, 1))) == 1
+						db "FW{x:TX_KATAKANA}_{char}"
+					ELIF INCHARMAP("FW{x:TX_HIRAGANA}_{char}")
 						IF cur_set != TX_HIRAGANA
 							DEF cur_set = TX_HIRAGANA
 							db cur_set
 						ENDC
-						db STRCAT("FW{x:TX_HIRAGANA}_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW0_", STRSUB(\1, i + 1, 1))) == 1
-						db STRCAT("FW0_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW1_", STRSUB(\1, i + 1, 1))) == 1
-						db TX_FULLWIDTH1, STRCAT("FW1_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW2_", STRSUB(\1, i + 1, 1))) == 1
-						db TX_FULLWIDTH2, STRCAT("FW2_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW3_", STRSUB(\1, i + 1, 1))) == 1
-						db TX_FULLWIDTH3, STRCAT("FW3_", STRSUB(\1, i + 1, 1))
-					ELIF CHARLEN(STRCAT("FW4_", STRSUB(\1, i + 1, 1))) == 1
-						db TX_FULLWIDTH4, STRCAT("FW4_", STRSUB(\1, i + 1, 1))
+						db "FW{x:TX_HIRAGANA}_{char}"
+					ELIF INCHARMAP("FW0_{char}")
+						db "FW0_{char}"
+					ELIF INCHARMAP("FW1_{char}")
+						db TX_FULLWIDTH1, "FW1_{char}"
+					ELIF INCHARMAP("FW2_{char}")
+						db TX_FULLWIDTH2, "FW2_{char}"
+					ELIF INCHARMAP("FW3_{char}")
+						db TX_FULLWIDTH3, "FW3_{char}"
+					ELIF INCHARMAP("FW4_{char}")
+						db TX_FULLWIDTH4, "FW4_{char}"
 					ELSE
-						FAIL STRCAT("Unmapped fullwidth character: ", STRSUB(\1, i + 1, 1))
+						FAIL "Unmapped fullwidth character: {char}"
 					ENDC
 				ENDR
 			ENDC
 		ENDC
 		SHIFT
 	ENDR
-	POPO
 ENDM
 
 MACRO textfw
