83f22cd55cea75629020c104d63427a395fe337d
dannye <33dannye@gmail.com>
2024-08-04 20:28:24 -0500

    Require rgbds v0.8.0

--- src/rgbdscheck.asm
+++ src/rgbdscheck.asm
@@ -1,6 +1,6 @@
 IF !DEF(__RGBDS_MAJOR__) || !DEF(__RGBDS_MINOR__) || !DEF(__RGBDS_PATCH__)
-	fail "poketcg requires rgbds v0.7.0 or newer."
+	fail "poketcg requires rgbds v0.8.0 or newer."
 ENDC
-IF __RGBDS_MAJOR__ == 0 && __RGBDS_MINOR__ <  7
-	fail "poketcg requires rgbds v0.7.0 or newer."
+IF __RGBDS_MAJOR__ == 0 && __RGBDS_MINOR__ < 8
+	fail "poketcg requires rgbds v0.8.0 or newer."
 ENDC
